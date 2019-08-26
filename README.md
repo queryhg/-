0682ddd929a0cdcb7dee97ed21826d9572967f4a
huanggang <1160631197@qq.com>
2019-08-26 10:00:23 +0800

    代码提交

---
+++ README.md
@@ -0,0 +1,30 @@
+# admin
+[微燕商城](https://github.com/jikeytang/vyan-shop-weixin) admin端
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import routes from './routes'
+import { getToken } from '@/utils/auth';
+// import store from '@/store'
+
+Vue.use(Router)
+
+const router = new Router({
+  routes
+})
+
+const whiteList = ['/login']
+
+router.beforeEach((to, from, next) => {
+  const token = getToken()
+
+  if (token) {
+    next()
+  } else {
+    if (whiteList.includes(to.path)) {
+      next()
+    } else {
+      next(`/login?redirect=${to.path}`)
+    }
+  }
+
+  next()
+})
+
+router.afterEach(to => {
+})
+
+export default router
